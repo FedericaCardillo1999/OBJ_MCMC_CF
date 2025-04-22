101b9857464dce864042cb476cd46641c3962796
Federica Cardillo <163155072+FedericaCardillo1999@users.noreply.github.com>
2025-04-22 15:32:31 +0200

    Fix on the source index filtering error

--- OBJ_MCMC_CF/CFandPRF.py
+++ OBJ_MCMC_CF/CFandPRF.py
@@ -22,20 +22,35 @@ def load_prf(subj, main_path, atlas, denoising, task):
     else:
         task_for_file = task
 
-    filepath = os.path.join(
-        main_path,
-        f'pRFM/{subj}/ses-02/{denoising}/model-manual-nelder-mead-GM_desc-prf_params_{task_for_file}.pkl')
-    pkl_data = pickle_file(filepath)
-    prf_params = pkl_data['model'].iterative_search_params
-    prf_voxels = np.where(pkl_data['rois_mask'] == 1)[0]
-    return prf_params, prf_voxels
+    # Determine model prefix based on atlas
+    if atlas.lower() == 'manualdelin' or atlas.lower() == 'manual':
+        model_prefix = 'model-manual-nelder-mead-GM_desc-prf_params'
+    elif atlas.lower() == 'benson':
+        model_prefix = 'model-benson-nelder-mead-GM_desc-prf_params'
+    else:
+        raise ValueError(f"Unsupported atlas type: {atlas}")
+
+    # Search both sessions
+    for ses in ['ses-01', 'ses-02']:
+        filepath = os.path.join(
+            main_path,
+            f'pRFM/{subj}/{ses}/{denoising}/{model_prefix}_{task_for_file}.pkl'
+        )
+        if os.path.exists(filepath):
+            pkl_data = pickle_file(filepath)
+            prf_params = pkl_data['model'].iterative_search_params
+            prf_voxels = np.where(pkl_data['rois_mask'] == 1)[0]
+            return prf_params, prf_voxels
+
+    raise FileNotFoundError(f"pRF .pkl file not found for subject {subj} with atlas '{atlas}' in any session.")
 
 def filter_prf(prf_params, prf_voxels):
     return PRFModel(
         r2=prf_params[:, 7],
         size=prf_params[:, 2],
         ecc=np.sqrt(prf_params[:, 1]**2 + prf_params[:, 0]**2),
-        angle=np.arctan2(prf_params[:, 1], prf_params[:, 0]))
+        angle=np.arctan2(prf_params[:, 1], prf_params[:, 0])
+    )
 
 def source_eccentricity(subj, hemi, main_path, atlas, denoising, task, freesurfer_path):
     prf_params, prf_voxels = load_prf(subj, main_path, atlas, denoising, task)
@@ -48,7 +63,7 @@ def source_eccentricity(subj, hemi, main_path, atlas, denoising, task, freesurfe
         adjusted_voxels = prf_voxels[prf_voxels >= numel_lh] - numel_lh
         ecc = prf_model.ecc[prf_voxels >= numel_lh]
     else:
-        adjusted_voxels = prf_voxels
-        ecc = prf_model.ecc
+        adjusted_voxels = prf_voxels[prf_voxels < numel_lh]
+        ecc = prf_model.ecc[prf_voxels < numel_lh]
 
     return dict(zip(adjusted_voxels, ecc))
